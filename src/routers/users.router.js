f3d3ae7b16e2f1537c68f3e2ce7e711f66f5bad6
Luciano Perez <jesus-61914@hotmail.com>
2023-07-28 14:12:01 -0600

    UPDATE: re-order the routes position

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -6,13 +6,13 @@ const user = new UserController()
 class UsersRouter extends RouterClass {
     init() {
         this.get('/', ['PUBLIC'], user.get)//Funciona
-        this.get('/:UID', ['PUBLIC'], user.getById)//Funciona
         this.post('/', ['PUBLIC'], user.post)//Funciona
+        this.get('/:UID', ['PUBLIC'], user.getById)//Funciona
+        this.put('/:UID', ['PUBLIC'], user.put)//Funciona
+        this.delete('/:UID', ['ADMIN'], user.delete)//Funciona
         this.post('/restore', ['PUBLIC'], user.restore)
         this.put('/restore/:UID', ['PUBLIC'], user.newPass)
         this.put('/premium/:UID', ['ADMIN'], user.updateRol)
-        this.put('/:UID', ['PUBLIC'], user.put)//Funciona
-        this.delete('/:UID', ['ADMIN'], user.delete)//Funciona
     }
 }
 module.exports = UsersRouter
