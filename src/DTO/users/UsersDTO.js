eae3f26f74b119029652e051be4fc65893e3633d
Luciano Perez <jesus-61914@hotmail.com>
2023-08-24 16:49:55 -0600

    UPDATE: Updated USER DTO

---
+++ src/DTO/users/UsersDTO.js
@@ -0,0 +1,21 @@
+class UserDto {
+    static #ageCalculator = (birthdate) => {
+        const age = new Date(birthdate).getFullYear();
+        const actualYear = new Date().getFullYear();
+        return actualYear - age;
+    }
+    static getUserDto = (user) => {
+        return {
+            UID: user._id.toString(),
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            CID: user.cartID.toString(),
+            age: this.#ageCalculator(user.birthdate),
+            role: user.role,
+            password: user.password
+        }
+    }
+}
+
+module.exports = UserDto
