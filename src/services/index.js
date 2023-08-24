eed50e9713d4dbfe01288228e016b59a304bb792
Luciano Perez <jesus-61914@hotmail.com>
2023-08-24 16:50:52 -0600

    ADDED: Session service to manage user session

--- src/services/index.js
+++ src/services/index.js
@@ -11,19 +11,22 @@ const ProductRepository = require('../repositories/product.repository.js')
 const CartRepository = require('../repositories/cart.repository.js');
 const TicketRepository = require("../repositories/ticket.repository.js");
 const ChatRepository = require('../repositories/chat.repo.js')
+const SessionRepository = require("../repositories/session.repository.js");
 
 const userService = new UserRepository(new UserDAO())
 const productService = new ProductRepository(new ProductDAO())
 const cartService = new CartRepository(new CartDAO())
 const ticketService = new TicketRepository(new TicketDAO())
 const chatService = new ChatRepository(new ChatDAO())
+const sessionService = new SessionRepository(new UserDAO(), new CartDAO())
 
 module.exports = {
     productService,
     userService,
     cartService,
     ticketService,
-    chatService
+    chatService,
+    sessionService
 }
 /*
 Al momento de yo cambiar la persistencia o la base de datos de mi proyecto, este archivo es el que se encarga de interconectar los controlladores con las nomenclaturas de la base de datos homogenizada
