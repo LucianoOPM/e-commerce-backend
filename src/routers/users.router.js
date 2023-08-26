278675a77bf1636d064ee96253ef3b1d30e11416
Luciano Perez <jesus-61914@hotmail.com>
2023-08-26 17:42:04 -0600

    ADDED: Little changes

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,19 +1,21 @@
 const UserController = require("../controllers/userv2.controller");
 const uploader = require("../middleware/multer");
+const validateToken = require("../middleware/validateUrlToken");
 const RouterClass = require("./RouterClass");
 
 const user = new UserController()
 
 class UsersRouter extends RouterClass {
     init() {
-        this.get('/', ['PUBLIC'], user.get)//Funciona
+        this.get('/', ['ADMIN'], user.get)//Funciona
         this.post('/', ['PUBLIC'], user.post)//Funciona
         this.get('/:UID', ['PUBLIC'], user.getById)//Funciona
         this.put('/:UID', ['PUBLIC'], user.put)//Funciona
+        this.delete('/', ['ADMIN'], user.deleteUsers)//funciona
         this.delete('/:UID', ['ADMIN'], user.delete)//Funciona
         this.post('/:UID/documents', ['USER', 'PREMIUM', 'ADMIN'], uploader.fields([{ name: "identify", maxCount: 1 }, { name: "address", maxCount: 1 }, { name: "bankStatement", maxCount: 1 }]), user.document)
         this.post('/restore', ['PUBLIC'], user.restore)
-        this.put('/restore/:UID', ['PUBLIC'], user.newPass)
+        this.put('/restore/:token', ['PUBLIC'], validateToken, user.newPass)
         this.put('/premium/:UID', ['ADMIN'], user.updateRol)
     }
 }
