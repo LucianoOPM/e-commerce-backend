265f80f4c34d84a90bbc15877d319bdcaaa162f0
Luciano Perez <jesus-61914@hotmail.com>
2023-09-01 12:54:45 -0600

    REMOVE: add product to cart functionality

--- src/public/js/viewProducts.js
+++ src/public/js/viewProducts.js
@@ -20,25 +20,3 @@ const logoutSession = (e) => {
 if (logoutBttn) {
     logoutBttn.addEventListener('click', logoutSession)
 }
-
-const addProduct = document.querySelector('#addButton')
-const cantidad = document.querySelector('#quantity')
-const productID = document.querySelector('#productID')
-
-addProduct.addEventListener('click', (e) => {
-    e.preventDefault()
-    const idCart = addProduct.value//el del carrito
-    const quantity = cantidad.value//cantidad a agregar
-    const idProduct = productID.innerText.split(" ")[1]//id del producto
-
-    fetch(`/api/carts/${idCart}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify([{ product: idProduct, quantity }], null)
-    })
-        .then(res => res.json())
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-})
