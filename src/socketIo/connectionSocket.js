31cd09517af704229d9a0e75d1b620b0c937af27
Luciano Perez <jesus-61914@hotmail.com>
2023-09-06 19:47:00 -0600

    REMOVED: middleware implementation

--- src/socketIo/connectionSocket.js
+++ src/socketIo/connectionSocket.js
@@ -1,21 +1,9 @@
-const { authUrl } = require("../config/passportJWT")
 const chatIo = require("./chat.io")
 const realTimeProductsIo = require("./realtimeproducts.io")
 
 const onConnection = (io) => {
   const socketHandler = async (socket) => {
     chatIo(io, socket)
-    io.use((socket, next) => {
-      const { handshake } = socket
-      const { cookie } = handshake.headers
-      const token = cookie.split('=')[1]
-      const user = authUrl(token)
-      if (user.role === 'ADMIN' || user.role === "premium") {
-        next()
-      } else {
-        next(new Error('no permission'))
-      }
-    })
     realTimeProductsIo(io, socket)
   }
   io.on('connection', socketHandler)
