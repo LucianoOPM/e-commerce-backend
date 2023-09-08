0e0fe7bfe1ac6ca40285fac51530b103ec4897fb
Luciano Perez <jesus-61914@hotmail.com>
2023-09-08 13:55:48 -0600

    ADDED: js file to chat

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,13 +1,21 @@
 const socket = io()
 const sendBtn = document.querySelector('#send-bttn')
 const text = document.querySelector('#text')
+const chat = document.querySelector('#chatArea')
 
 sendBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    socket.emit('handshake', "hola mundo")
+    const message = text.value
+    socket.emit('client:message', message)
     text.value = ""
 })
 
-socket.on('message', (data) => {
-    console.log(data)
+socket.on('server:message', async (data) => {
+    const messageFormat = `${data.first_name} says: ${data.message}`
+
+    chat.innerHTML += `${messageFormat}\n`
+})
+
+socket.on('connect_error', (err) => {
+    console.log(err.message)
 })
